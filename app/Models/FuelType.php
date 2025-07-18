9fd43206ebc2834d26c23730900e6978aa64a76e
Unr1ll <x3narontyprojects@gmail.com>
2025-07-18 22:29:05 +0200

    feat: add models for database schema

---
+++ app/Models/FuelType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FuelType extends Model
+{
+    use HasFactory;
+    
+    // protected $table = 'car_fuel_types'; // custom table name
+    // protected $primaryKey = 'fuel_type_id'; // custom primary key
+    // public $incrementing = false; // disable auto incrementing
+    // protected $keyType = 'string'; // primary key type    
+}
