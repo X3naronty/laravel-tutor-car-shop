6a8307d6f3949d7111dce4dd020a41e7874e8124
Unr1ll <x3narontyprojects@gmail.com>
2025-07-22 23:05:29 +0200

    fix(cars table): column redundancy

--- database/migrations/2025_07_22_191544_create_cars_table.php
+++ database/migrations/2025_07_22_191544_create_cars_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('maker_id')->constrained('makers');
             $table->foreignId('model_id')->constrained('models');
             $table->integer('year');
             $table->integer('price');
@@ -25,8 +24,6 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('city_id')->constrained('cities');
 
-            $table->string('address', 255);
-            $table->string('phone', 45);
             $table->longText('description')->nullable();
             $table->timestamp('published_at');
             $table->timestamp('deleted_at')->nullable();
