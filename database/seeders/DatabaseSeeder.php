29586a66d64f6346b852894df2d728b82d80f84a
Unr1ll <x3narontyprojects@gmail.com>
2025-08-01 16:24:55 +0200

    feat(DatabaseSeeder): add main database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Car;
+use App\Models\CarImage;
+use App\Models\CarType;
+use App\Models\City;
+use App\Models\FuelType;
+use App\Models\Maker;
+use App\Models\Model;
+use App\Models\State;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +21,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $carTypes = require database_path('seeders/data/car_types.php');
+        $carTypesCount = count($carTypes);
+        CarType::factory()
+            ->sequence(...$carTypes)->count($carTypesCount)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $fuelTypes = require database_path('seeders/data/fuel_types.php');
+        $fuelTypesCount = count($fuelTypes);
+        FuelType::factory()->sequence(...$fuelTypes)->count($fuelTypesCount)->create();
+
+        $statesWithCities = require database_path('seeders/data/states_with_cities.php');
+        foreach ($statesWithCities as $stateName => $cities) {
+            $citiesCount = count($cities);
+            State::factory()->has(City::factory()->count($citiesCount)->sequence(...array_map(fn($city) => ['name' => $city], $cities)))->create(['name' => $stateName]);
+        }
+
+        $makersWithModels = require database_path('seeders/data/makers_with_models.php');
+        foreach ($makersWithModels as $makerName => $models) {
+            $modelsCount = count($models);
+            Maker::factory()->has(Model::factory()->count($modelsCount)->sequence(...array_map(fn($model) => ['name' => $model], $models)))->create(['name' => $makerName]);
+        }
+
+        User::factory()->count(3)->create();
+        User::factory()->count(2)->has(
+            Car::factory()->count(50)->has(
+                CarImage::factory()->count(5)->sequence(
+                    ['position' => 1],
+                    ['position' => 2],
+                    ['position' => 3],
+                    ['position' => 4],
+                    ['position' => 5],
+                )
+            )
+            ->hasFeatures(),
+            'favouriteCars'
+        )->create();
     }
 }
